39995e3f2a415cf728ecf54d71feb4ee75b61b59
AnkonChy <bannah76769@gmail.com>
2025-12-12 15:35:30 +0600

    db.js

--- src/configure/db.js
+++ src/configure/db.js
@@ -1,4 +1,33 @@
 
+// import dotenv from 'dotenv'
+
+// import mongoose from "mongoose"
+// dotenv.config();
+
+// const connectDB = async()=>{
+
+
+// try {
+//     const connect = await mongoose.connect('mongodb+srv://next_goal:zcQBM09IcEXIRfSc@cluster0.vsivf.mongodb.net/?appName=Cluster0', {
+//         dbName: 'next-goal', 
+//       });
+//    console.log(`moongdo db connected `);
+    
+// } catch (error) {
+//     console.log(error.message, "error");
+// }
+// }
+
+// export default connectDB;
+
+
+
+
+
+
+
+
+
 import dotenv from 'dotenv'
 
 import mongoose from "mongoose"
@@ -8,7 +37,7 @@ const connectDB = async()=>{
 
 
 try {
-    const connect = await mongoose.connect('mongodb+srv://next_goal:zcQBM09IcEXIRfSc@cluster0.vsivf.mongodb.net/?appName=Cluster0', {
+    const connect = await mongoose.connect('mongodb://localhost:27017/', {
         dbName: 'next-goal', 
       });
    console.log(`moongdo db connected `);
@@ -19,12 +48,3 @@ try {
 }
 
 export default connectDB;
-
-
-
-
-
-
-
-
-
